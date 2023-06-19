fda8b3562ad3e38db0bfc79cfc898a32cc48883f
+ <+>
2023-06-19 14:35:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName =customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const LeastFavoriteCustomer = 'Lofi'
+function changeLeastFavoriteCustomer() {
+    LeastFavoriteCustomer = 'Filo'
+
+    if (LeastFavoriteCustomer!==Lofi) {
+        return 'Assignment to constant variable.'
+    }
+}
+    
+
